570ce9e9cb9dfe991f4447b1464a41325940f00c
Kostas Andreadis <kmandreadis@gmail.com>
2019-04-30 09:32:44 -0400

    Calculate discharge prior distribution

--- src/Sads.jl
+++ src/Sads.jl
@@ -1,6 +1,7 @@
 module Sads
 
 using Distributions
+using KernelDensity: kde
 
 abstract type CrossSection end
 
@@ -29,8 +30,30 @@ end
 Estimate the prior probability distribution of discharge.
 
 """
-function discharge_prior()
-
+function discharge_prior(qwbm, nens, nsamples, H, W, x, nₚ, rₚ, zₚ)
+    S = diff(H, dims=1) ./ diff(x)
+    S = [S[1, :]'; S]
+    obs = [std(sample(H[end, :], nens)) for _ in 1:nsamples]
+    Fobs = kde(obs)
+    h = zeros(nsamples)
+    δ = lhs_ensemble(nsamples, Uniform(0.5, 2.0))[1]
+    t = sample(collect(1:size(H, 2)), nsamples)
+    for s in 1:nsamples
+        μ = log(qwbm / sqrt(1 + δ[s]^2))
+        σ = log(1 + δ[s]^2)
+        Qₚ = LogNormal(μ, σ)
+        Qe, ne, re, ze = prior_ensemble(x, Qₚ, nₚ, rₚ, zₚ, nens)
+        he = gvf_ensemble!(H[:, t[s]], W[:, t[s]], S[:, t[s]], x, maximum(H, dims=2), maximum(W, dims=2),
+                          Qe, [mean(ne) for i in 1:nens], [mean(re) for i in 1:nens], ze)
+        i = findall(he[1, :] .> 0)
+        h[s] = std(he[end, i] .* (mean(re) .+ 1) ./ mean(re) .+ ze[end, i])
+    end
+    δ = δ[.!isnan.(h)]
+    h = h[.!isnan.(h)]
+    Fmod = kde(h)
+    L = 1
+    accepted = [rand(Uniform(0, L)) * pdf(Fmod, s) <= pdf(Fobs, s) for s in  h]
+    mean(δ[accepted]), std(δ[accepted])
 end
 
 """
