62e7a7a088616001a971c92978a51d18e6e55fef
Kostas Andreadis <kandread@umass.edu>
2019-06-10 10:15:23 -0400

    Write SWOT parameters to NetCDF file

--- scripts/swot.jl
+++ scripts/swot.jl
@@ -4,6 +4,22 @@ using Sads
 using Distributions
 using NCDatasets
 
+function load_severn(ncfile)
+    ds = Dataset(ncfile)
+    xs = ds.group["XS_Timseries"]
+    W = xs["W"][:]; W = W[end:-1:1, :]
+    H = xs["H_90m"][:]; H = H[end:-1:1, :]
+    x = collect(0.0:90.0:(size(H, 1)-1)*90)
+    ri = [1;length(x)]
+    Q = xs["Q"][:]; Q = Q[end:-1:1, :]
+    qwbm = mean(Q[Q .> 0])
+    close(ds)
+    Q = Q[:, findall(all(.!isnan.(H), dims=1)[1,: ])]
+    W = W[:, findall(all(.!isnan.(H), dims=1)[1,: ])]
+    H = H[:, findall(all(.!isnan.(H), dims=1)[1,: ])]
+    Q, H, W, x, qwbm, ri
+end
+
 "Load hydraulic variables from NetCDF file."
 function load_data(ncfile)
     ds = Dataset(ncfile)
@@ -23,26 +39,36 @@ function load_data(ncfile)
 end
 
 "Write output data to NetCDF file."
-function write_data(ncfile, Qa)
-    ds = Dataset(ncfile, "a")
-    xs = NCDatasets.group(ds, "XS_Timeseries")
-    ri = NCDatasets.group(ds, "River_Info")
-    rc = NCDatasets.group(ds, "Reach_Timeseries")
-    rbnd = ri["rch_bnd"][:]
-    x = xs["X"][:, 1]
-    ri = [findmin(sqrt.((r .- x).^2))[2] for r in rbnd]
-    x = x[end] .- x; x = x[end:-1:1]
-    ri = length(x) .- ri .+ 1; ri = ri[end:-1:1]
-    Qo = zeros(length(x), size(Qa, 2))
-    for r in 1:length(ri)-1
-        for rr in ri[r]:ri[r+1]-1
-            Qo[rr, :] .= Qa[r, :]
-        end
-    end
-    Qxs = xs["Q"]
-    Qxs[:] = Qo[end:-1:1, :]
-    Qr = rc["Q"]
-    Qr[:] = Qa[end:-1:1, :]
+function write_data(ncfile, H, W, x, ri, Qa, A0, n)
+    nr, nt = size(Qa)
+    ds = Dataset(ncfile, "c")
+    defDim(ds, "reach", nr)
+    defDim(ds, "time", nt)
+    Hr = [mean(H[ri[j]:ri[j+1], t]) for j in 1:nr, t in 1:nt]
+    Wr = [mean(W[ri[j]:ri[j+1], t]) for j in 1:nr, t in 1:nt]
+    Sr = [(H[ri[j+1], t] - H[ri[j], t]) / (x[ri[j+1]] - x[ri[j]]) for j in 1:nr, t in 1:nt]
+    Hvar = defVar(ds, "H", Float32, ("reach", "time"))
+    Hvar.attrib["units"] = "m"
+    Hvar.attrib["comments"] = "Water Surface Elevation"
+    Wvar = defVar(ds, "W", Float32, ("reach", "time"))
+    Wvar.attrib["units"] = "m"
+    Wvar.attrib["comments"] = "Width"
+    Svar = defVar(ds, "S", Float32, ("reach", "time"))
+    Svar.attrib["units"] = "m/m"
+    Svar.attrib["comments"] = "Water Surface Slope"
+    Qvar = defVar(ds, "Q", Float32, ("reach", "time"))
+    Qvar.attrib["units"] = "m3/s"
+    Qvar.attrib["comments"] = "Discharge"
+    nvar = defVar(ds, "n", Float32, ("reach", "time"))
+    nvar.attrib["units"] = "unitless"
+    nvar.attrib["comments"] = "Manning's roughness"
+    A0var = defVar(ds, "A0", Float32, ("reach", "time"))
+    Hvar[:, :] = Hr
+    Wvar[:, :] = Wr
+    Svar[:, :] = Sr
+    Qvar[:, :] = Qa
+    A0var[:, :] = A0
+    nvar[:, :] = n
     close(ds)
 end
 
@@ -65,9 +91,9 @@ end
 function main()
     ncfile = ARGS[1]
     rₚ = eval(Symbol(ARGS[2]))(parse(Float64, ARGS[3]), parse(Float64, ARGS[4]))
-    Q, H, W, x, qwbm, ri = load_data(ncfile)
-    Qa = Sads.estimate(qwbm, H, W, x, rₚ, ri)
-    write_data(ncfile, Qa)
+    Q, H, W, x, qwbm, ri = load_severn(ncfile)
+    Qa, A0, n = Sads.estimate(qwbm, H, W, x, rₚ, ri)
+    write_data("sads_$ncfile", H, W, x, ri, Qa, A0, n)
 end
 
 main()
