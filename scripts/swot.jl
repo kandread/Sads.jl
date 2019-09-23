8750650e32439b4d5696885ffebe10dc8421ad5f
Kostas Andreadis <kandread@umass.edu>
2019-09-23 15:06:43 -0400

    Run Confluence estimation

--- scripts/swot.jl
+++ scripts/swot.jl
@@ -31,10 +31,19 @@ function read_data(ncfile)
     W = xs["W"][:]
     H = xs["H_1km"][:]
     Q = xs["Q"][:]
+    S = abs.(NCDatasets.group(ds, "Reach_Timeseries")["S_90m"][:]) ./ length(x)
     close(ds)
-    j = findall(sum(isnan.(H), dims=1)[1, :] .== 0)
-    H = sort(H[:, j], dims=1)
+    j = findall((sum(isnan.(H), dims=1)[1, :] .== 0) .& (all(W .> 0, dims=1)[1, :]))
+    # H = sort(H[:, j], dims=1)
     W = W[:, j]; W = W[end:-1:1, :]
+    Hmin = minimum(H[:, j], dims=1)
+    H = zeros(size(W))
+    H[1, :] = Hmin
+    for c in 2:size(H, 1)
+        for t in 1:size(H, 2)
+            H[c, t] = H[c-1, t] + S[t] .* (x[c] - x[c-1])
+        end
+    end
     Q = Q[:, j]; Q = Q[end:-1:1, :]
     qwbm = mean(abs.(Q))
     ri = [1; length(x)]
@@ -42,14 +51,11 @@ function read_data(ncfile)
 end
 
 "Write output data to NetCDF file."
-function write_data(ncfile, H, W, x, ri, Qa, A0, n)
+function write_data(ncfile, H, W, S, Qa, A0, n)
     nr, nt = size(Qa)
     ds = Dataset(ncfile, "c")
     defDim(ds, "nx", nr)
     defDim(ds, "nt", nt)
-    Hr = [mean(H[ri[j]:ri[j+1], t]) for j in 1:nr, t in 1:nt]
-    Wr = [mean(W[ri[j]:ri[j+1], t]) for j in 1:nr, t in 1:nt]
-    Sr = [(H[ri[j+1], t] - H[ri[j], t]) / (x[ri[j+1]] - x[ri[j]]) for j in 1:nr, t in 1:nt]
     Hvar = defVar(ds, "H", Float32, ("nx", "nt"))
     Hvar.attrib["units"] = "m"
     Hvar.attrib["comments"] = "Water Surface Elevation"
@@ -62,16 +68,16 @@ function write_data(ncfile, H, W, x, ri, Qa, A0, n)
     Qvar = defVar(ds, "Q", Float32, ("nx", "nt"))
     Qvar.attrib["units"] = "m3/s"
     Qvar.attrib["comments"] = "Discharge"
-    nvar = defVar(ds, "n", Float32, ("nx", "nt"))
+    nvar = defVar(ds, "n", Float32, ("nx",))
     nvar.attrib["units"] = "unitless"
     nvar.attrib["comments"] = "Manning's roughness"
-    A0var = defVar(ds, "Abase", Float32, ("nx", "nt"))
-    Hvar[:, :] = Hr
-    Wvar[:, :] = Wr
-    Svar[:, :] = Sr
+    A0var = defVar(ds, "Abase", Float32, ("nx",))
+    Hvar[:, :] = H
+    Wvar[:, :] = W
+    Svar[:, :] = S
     Qvar[:, :] = Qa
-    A0var[:, :] = A0
-    nvar[:, :] = n
+    A0var[:] = A0
+    nvar[:] = n
     close(ds)
 end
 
@@ -95,17 +101,22 @@ function main()
     ncpath = ARGS[1]
     rₚ = eval(Symbol(ARGS[2]))(parse(Float64, ARGS[3]), parse(Float64, ARGS[4]))
     ncfiles = joinpath.(abspath(ncpath), readdir(ncpath))
-    Qa = [Float64[] for _ in 1:length(ncfiles)]
-    A0 = [0.0 for _ in 1:length(ncfiles)]
-    n = [0.0 for _ in 1:length(ncfiles)]
-    H = Array{Array{Float64, 2}, 1}(undef, length(ncfiles))
-    W = Array{Array{Float64, 2}, 1}(undef, length(ncfiles))
+    nt = maximum([length(Dataset(ncfile)["Time steps"]) for ncfile in ncfiles])
+    Qa = zeros(length(ncfiles), nt)
+    A0 = zeros(length(ncfiles))
+    n = zeros(length(ncfiles))
+    H = zeros(length(ncfiles), nt)
+    W = zeros(length(ncfiles), nt)
+    S = zeros(length(ncfiles), nt)
     for (i, ncfile) in enumerate(ncfiles)
-        Q, H[i], W[i], x, qwbm, ri = read_data(ncfile)
-        qa, a0, mann = Sads.estimate(qwbm, H[i], W[i], x, rₚ, ri)
-        Qa[i], A0[i], n[i] = qa[1, :], mean(a0), mean(mann)
+        Q, h, w, x, qwbm, ri = read_data(ncfile)
+        qa, a0, mann = Sads.estimate(qwbm, h, w, x, rₚ, ri)
+        H[i, 1:size(h, 2)] = mean(h, dims=1)
+        W[i, 1:size(w, 2)] = mean(w, dims=1)
+        S[i, 1:size(w, 2)] = mean(diff(h, dims=1) ./ diff(x), dims=1)
+        Qa[i, 1:size(h, 2)], A0[i], n[i] = mean(qa, dims=1), a0, mann
     end
-    write_data("sads_output.nc", H, W, Qa, A0, n)
+    write_data("sads_output.nc", H, W, S, Qa, A0, n)
 end
 
 main()
